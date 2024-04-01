69dc69ec3bb6f838e723bc03f73c233e5246cc06
zeref <krishh16@yahoo.com>
2024-04-01 18:53:07 +0530

    added backend logic

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken"
+
 const prisma = new PrismaClient();
 const app = express();
 
+
+const jwtSecret = "somethingCrazyAsHell"
 app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -27,7 +31,7 @@ app.post("/signup", async (req: any, res: any) => {
     })
     console.log(userExists)
     if (userExists) {
-        return res.status(403).json({msg: "user already exists with the following credentials", success: false})
+        return res.status(403).json({msg: "user already exists with the following credentials", success:false})
     }
 
     await prisma.user.create(
@@ -40,9 +44,56 @@ app.post("/signup", async (req: any, res: any) => {
             }
         }
     )
-        console.log('user created')
+    console.log('user created')
     res.status(200).json({msg: "User Created Successfully", success: true})
 
+})
+
+app.post("/signin",async (req, res) => {
+    const {email, password} = req.body;
+    
+    try {
+        // Check if email and password are provided
+        if (!email || !password) {
+          return res.status(400).json({ error: 'Email and password are required' });
+        }
+    
+        // Check if user exists
+        const existingUser = await prisma.user.findFirst({
+          where: { email }
+        });
+    
+        if (!existingUser) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+    
+        // Compare passwords
+        const passwordMatch = existingUser.password == password;
+    
+        if (!passwordMatch) {
+          return res.status(401).json({ error: 'Invalid password' });
+        }
+        
+        jwt.sign({email}, jwtSecret, (err: any, token: any) => {
+            if (err) {
+                console.error("Error generating JWT:", err)
+                return res.status(500).json({err: "Internal server error"})
+            }
+            res.cookie("userToken", token, {
+                sameSite: "strict"
+            })
+            console.log('user token created')
+            res.json({token})
+        })
+
+        
+        
+
+    } catch (error) {
+        console.error('Error during sign-in:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    
 })
 
 app.listen(3001, () => {
