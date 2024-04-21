d1b221cd75d22cba627c8eb88374cabb6285a9bd
harshana15 <harshana555prajapati@gmail.com>
2024-04-21 16:43:34 +0530

    Midway implementation of onboardMCQs

---
+++ apps/web/src/app/api/onboardmcqs/route.ts
@@ -0,0 +1,8 @@
+import prisma from "../../../../../../packages/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req:NextRequest){
+    const userChoice:{
+
+    } = await req.json();
+} 
--- apps/web/src/app/onboard/page.tsx
+++ apps/web/src/app/onboard/page.tsx
@@ -1,85 +1,84 @@
 "use client"
 import './onboard.css'
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function onBoard() {
-    const [userChoice, setChoice] =useState({
-        interest: "",
-        experience: "",
-        social: "",
-        socialAwareness: "",
-        resume: ""
-    });
-    const updateChoice = (key, value) => {
-        setChoice(prevState => ({
-            ...prevState,
-            [key]: value
-        }));
-    };
+  const [userChoice, setChoice] = useState({});
+  const updateChoice = (key, value) => {
+    setChoice(prevState => ({
+      ...prevState,
+      [key]: value
+    }));
+  };
 
-    const handleSubmit =(e) => {
-        e.preventDefault();
-        console.log(userChoice)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(userChoice);
+    try{
+      const response = await axios.post("http://localhost:3000/onboardmcqs", userChoice);
+      console.log(response);
+      if(!response.data.success){
+        alert("Onboard mcqs submission failed!")
+      }
+    }catch(err){
+      console.log(err);
     }
+  }
 
-    const arr = [{
-    legend: 'What interests you?', 
+  const arr = [{
+    legend: 'What interests you?',
     options: [
-    {id: "1", name: "interest", value:"AI/ML/DL"},
-    {id: "2", name: "interest", value:"Web Dev"},
-    {id: "3", name: "interest", value:"BlockChain"},
-    {id: "4", name: "interest", value:"DevOps"}]
+      { id: "1", value: "AI/ML/DL" },
+      { id: "2", value: "Web Dev" },
+      { id: "3", value: "BlockChain" },
+      { id: "4", value: "DevOps" }]
   },
   {
-    legend: 'How experienced are you?', 
+    legend: 'How experienced are you?',
     options: [
-    {id: "5", name: "experience", value:"Just starting out"},
-    {id: "6", name: "experience", value:"Intermediate"},
-    {id: "7", name: "experience", value:"Advanced"}
-  ]
+      { id: "5",value: "Just starting out" },
+      { id: "6",value: "Intermediate" },
+      { id: "7",value: "Advanced" }
+    ]
   },
   {
-    legend: 'How active are you on twitter?', 
+    legend: 'How active are you on twitter?',
     options: [
-    {id: "8", name: "social", value:"Not at all, just made the account"},
-    {id: "9", name: "social", value:"Posting seldomly"},
-    {id: "10", name: "social", value:"Posting aggressively"}
-  ]
+      { id: "8", name: "social", value: "Not at all, just made the account" },
+      { id: "9", name: "social", value: "Posting seldomly" },
+      { id: "10", name: "social", value: "Posting aggressively" }
+    ]
   },
   {
-    legend: "How crucial is social presence and #buildinpublic for you in terms of attracting new opportunities?", 
+    legend: "How crucial is social presence and #buildinpublic for you in terms of attracting new opportunities?",
     options: [
-    {id: "11", name: "socialAwareness", value:"Really Important, I'm working on it."},
-    {id: "12", name: "socialAwareness", value:"Think it's important but not taking actionable-steps"},
-    {id: "13", name: "socialAwareness", value:"Never thought about it."}
-  ]
+      { id: "11",value: "Really Important, I'm working on it." },
+      { id: "12",value: "Think it's important but not taking actionable-steps" },
+      { id: "13",value: "Never thought about it." }
+    ]
   },
   {
-    legend: "Do you have a resume?", 
+    legend: "Do you have a resume?",
     options: [
-    {id: "14", name: "resume", value:"Yes"},
-    {id: "15", name: "resume", value:"No, but I'll make one soon"}
-  ]
+      { id: "14",value: "Yes" },
+      { id: "15",value: "No, but I'll make one soon" }
+    ]
   }]
-  const content = arr.map(item => {
+  const content = arr.map((item,queIndex) => {
     const options = item.options.map(option => {
-        return (
-            <div className="form__group">
-                <input onClick={(e) => {
-                        updateChoice(e.target.name, e.target.value)
-                    
-                }} type="checkbox" key={option.id} id={option.id} value={option.value} name={option.name}/>
-                <label htmlFor={option.id}> {option.value} </label>
-            </div>
-        )
+      return (
+        <div key={option.id} className="form__group">
+          <input type="radio" id={option.id} name={item.legend} value={option.value} checked={userChoice[item.legend]===option.value} onChange={()=> updateChoice(item.legend,option.value)} required/>
+          <label htmlFor={option.id}> {option.value} </label>
+        </div>
+      )
     })
     return (
-        
-                <fieldset className="border border-black border-solid p-3 rounded-md" >
-                    <legend className='pr-2.5 pl-1' >{item.legend}</legend>
-                    {options}
-                </fieldset>
-      
+      <fieldset key={queIndex} className="border border-black border-solid p-3 rounded-md" >
+        <legend className='pr-2.5 pl-1' >{item.legend}</legend>
+        {options}
+      </fieldset>
     )
   })
 
@@ -87,16 +86,15 @@ export default function onBoard() {
     <>
       <div className="wrapper">
         <h1>Please answer a few quick questions</h1>
-        <div className="grid" style={{color: 'black'}}>
-        <form action="">
-           {content}
-           <button type='submit' onSubmit={(e) => handleSubmit(e)} className='h-8 w-8 gap-1 md:w-auto border py-1 px-2 duration-200 hover:bg-gray-100 rounded-lg text-xs all-center'>
-            Submit
-           </button>
-           
-           </form>
+        <div className="grid" style={{ color: 'black' }}>
+          <form  onSubmit={(e) => handleSubmit(e)} >
+            {content}
+            <button type='submit'className='h-8 w-8 gap-1 md:w-auto border border-black py-1 px-2 duration-200 hover:bg-gray-100 rounded-lg text-xs all-center'>
+              Submit
+            </button>
+          </form>
         </div>
-     </div>
+      </div>
     </>
   );
 };
